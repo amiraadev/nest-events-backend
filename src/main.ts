dbde8fc7cdcd3049dabf4306d83c2229e5f9d9ed
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-05 20:05:02 +0100

    logger: ['error', 'warn', 'debug'],

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
